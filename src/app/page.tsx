0425fe66a4363e8f1c5088777b54e3518d301a5d
Carlos Benavides <carlosbvz@gmail.com>
2024-11-13 18:51:21 -0600

    adding mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@mui/material'
+
 export default function Home() {
-    return <div>Page</div>
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    )
 }
